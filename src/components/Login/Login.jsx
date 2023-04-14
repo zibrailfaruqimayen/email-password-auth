2a11ee0ac999b8538e1c2aaee1de4d8b622da6fc
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-04-14 16:45:40 +0600

    signin form added

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,12 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email, password);
+
+    // velidation
+    setError("");
+    setSuccess("");
+
+    if (!/(?=.*[A-Z].*[A-Z])/.test(password)) {
+      setError("Please add at least tow uppercase.");
+      return;
+    } else if (!/(?=.*[!@#$&*]) /.test(password)) {
+      setError("Please add a special character.");
+      return;
+    } else if (password.length < 6) {
+      setError("password must br 6 characters long");
+      return;
+    }
+  };
+
   return (
-    <div>
-      <h2>login</h2>
+    <div className="w-25 mx-auto mt-5">
+      <h2>Please Login</h2>
+      <form onSubmit={handleLogin}>
+        <div className="form-group mb-3">
+          <label htmlFor="email">Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            name="email"
+            id="email"
+            placeholder="Enter email"
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+
+        <div className="form-group mb-3">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            className="form-control"
+            name="password"
+            id="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-check mb-3">
+          <input
+            type="checkbox"
+            className="form-check-input"
+            id="rememberMe"
+            onChange={(e) => setRememberMe(e.target.checked)}
+          />
+          <label className="form-check-label" htmlFor="rememberMe">
+            Remember Me
+          </label>
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+      <p className="text-danger">{error}</p>
+      <p className="text-success">{success}</p>
     </div>
   );
 };
 
 export default Login;
-<h2>login</h2>;
