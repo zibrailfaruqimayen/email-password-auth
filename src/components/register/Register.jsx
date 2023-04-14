22b80ce1a5b7b0e0d3455c9a1fcbfa16af72488f
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-04-14 15:25:42 +0600

    password valedation added

--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -5,26 +5,51 @@ import app from "../../firebase/firebase.config";
 const auth = getAuth(app);
 
 const Register = () => {
-  const [email, setEmail] = useState("");
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
 
   const handleEmailChange = (event) => {
-    setEmail(event.target.value);
+    // setEmail(event.target.value);
     // console.log(event.target.value);
   };
 
   const handleSubmit = (event) => {
+    // 1. prevent page refresh
     event.preventDefault();
+    setSuccess("");
+    setError("");
+
+    // 2. collect form data
     const email = event.target.email.value;
     const password = event.target.password.value;
+
+    // validate
+    if (!/(?=.*[A-Z]) /.test(password)) {
+      setError("Please add at least one uppercase");
+      return;
+    } else if (!/(?=.*[0-9].*[0-9]) /.test(password)) {
+      setError("Please add at least two numbers");
+      return;
+    } else if (password.length < 6) {
+      setError("Please add at least 6 character in your password");
+      return;
+    }
+
     console.log(email, password);
+
     // create user in fb
     createUserWithEmailAndPassword(auth, email, password)
       .then((result) => {
         const loggedUser = result.user;
+        setError("");
+        event.target.reset();
+        setSuccess("User has been created successfully");
         console.log(loggedUser);
       })
       .catch((error) => {
-        console.log(error);
+        setError(error.message);
+
+        // console.log(error);
       });
   };
 
@@ -38,6 +63,7 @@ const Register = () => {
         <input
           className="w-50 mb-4 rounded p-2"
           onChange={handleEmailChange}
+          required
           type="email"
           name="email"
           id="email"
@@ -47,6 +73,7 @@ const Register = () => {
         <input
           className="w-50 mb-4 rounded p-2"
           onBlur={handlePasswordBlur}
+          required
           type="password"
           name="password"
           id="password"
@@ -55,6 +82,8 @@ const Register = () => {
         <br />
         <input className="btn btn-primary" type="submit" value="Register" />
       </form>
+      <p className="text-danger">{error}</p>
+      <p className="text-success">{success}</p>
     </div>
   );
 };
